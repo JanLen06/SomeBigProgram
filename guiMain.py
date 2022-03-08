f81ce33c7d60b4e28f9c99d7fbf0f9236940225e
JanLen06 <jan@lennertz.eu>
2022-03-08 16:14:50 +0100

    Starting the GUI

--- guiMain.py
+++ guiMain.py
@@ -1,1 +1,12 @@
 #the file to manage all of the GUI
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+root.geometry("800x800")
+
+frame = ttk.Frame(root, padding=10)
+frame.grid()
+
+root.mainloop()
